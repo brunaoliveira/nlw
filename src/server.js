d50e55b11075c00bdee3b5782981e948ddcfcc64
brunaoliveira <brunaol678@gmail.com>
2020-08-07 16:02:31 -0300

    refactor server functionalities

--- src/server.js
+++ src/server.js
@@ -1,9 +1,46 @@
+const proffys = [
+    {
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "0123456789", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [729], 
+        time_to: [1220]
+    },
+    {
+        name: "Daniele Evangelista", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "0123456789", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [729], 
+        time_to: [1220]
+    }
+]
+
+function pageLanding(req, res ) {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+}
+
+function pageStudy(req, res) {
+    return res.sendFile(__dirname + "/views/study.html")
+}
+
+function giveClasses(req, res) {
+    return res.sendFile(__dirName + "/views/give-classes.html")
+}
+
 const express = require('express')
 const server = express()
 
 server.use(express.static("public"))
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", giveClasses)
 .listen(5500)
 // console.log(__dirname)
