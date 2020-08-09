5b067769e9d2844d4a2efad590801cec1cf657ca
brunaoliveira <brunaol678@gmail.com>
2020-08-09 19:00:07 -0300

    loops to populate html forms from backend data

--- src/server.js
+++ src/server.js
@@ -11,6 +11,17 @@ const proffys = [
         time_to: [1220]
     },
     {
+        name: "Mayk Brito", 
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
+        whatsapp: "51999082779", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [729], 
+        time_to: [1220]
+    },
+    {
         name: "Daniele Evangelista", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
         whatsapp: "0123456789", 
@@ -23,13 +34,27 @@ const proffys = [
     }
 ]
 
-function pageLanding(req, res ) {
+const subjects = [
+    "Biologia",
+    "Artes",
+    "Ciências",
+    "Educação",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+
+function pageLanding(req, res) {
     // sem nunjucks: return res.sendFile(__dirname + "/views/index.html")
     return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    filters = req.query
+    return res.render("study.html", {proffys, subjects, filters})
 }
 
 function giveClasses(req, res) {
